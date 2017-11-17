9e7e592a61405bbd4a9d8c3e235bbc3fdec1ffe8
mi-24v <miwpayou0808@gmail.com>
2017-11-17 15:20:57 +0900

    傾き取得機能改装

--- app/src/main/java/jissen/e/jissennohito/MainActivity.java
+++ app/src/main/java/jissen/e/jissennohito/MainActivity.java
@@ -13,60 +13,64 @@ import android.hardware.usb.UsbManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import java.util.Collection;
-import java.util.HashMap;
-
 public class MainActivity extends Activity {
     private SensorManager mSensorManager;
-    private Sensor mAccelerometer,mMagneticField;
+    private Sensor mAccelerometer, mMagneticField;
     private SensorEventListener mSensorListener;
 
     private UsbDevice usbDevice;
-//    private UsbManager mUsbManager;
+    //    private UsbManager mUsbManager;
     private static final String ACTION_USB_PERMISSION = "jissen.e.jissennohito.USB_PERMISSION";
-
-    private float[] rotationMatrix = new float[9];
-    private float[] gravity = new float[3];
-    private float[] geomagnetic = new float[3];
-    private float[] attitude = new float[3];
+    /**
+     * rotation data set
+     */
+    private RotationData rotationData = RotationData.INSTANCE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mSensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         mMagneticField = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
         mSensorListener = new SensorEventListener() {
             @Override
             public void onSensorChanged(SensorEvent event) {
-                switch (event.sensor.getType()){
-                    case Sensor.TYPE_ACCELEROMETER:
-                        geomagnetic = event.values.clone();
-                        break;
+                //精度が悪いときは捨てる
+                if (event.accuracy <= SensorManager.SENSOR_STATUS_UNRELIABLE) return;
+                //取得
+                switch (event.sensor.getType()) {
                     case Sensor.TYPE_MAGNETIC_FIELD:
-                        gravity = event.values.clone();
+                        rotationData.setGeomagnetic(event.values.clone());
+                        break;
+                    case Sensor.TYPE_ACCELEROMETER:
+                        rotationData.setAcceleration(event.values.clone());
                         break;
                 }
-                if (geomagnetic != null && gravity != null) {
-                    SensorManager.getRotationMatrix(rotationMatrix, null, gravity, geomagnetic);
-                    SensorManager.getOrientation(rotationMatrix, attitude);
+                if (rotationData.getGeomagnetic() != null && rotationData.getAcceleration() != null) {
+                    float[] rotationMatrix = new float[9],
+                            remappedRotationMatrix = new float[9];
+                    SensorManager.getRotationMatrix(rotationMatrix, null, rotationData.getAcceleration(), rotationData.getGeomagnetic());
+                    SensorManager.remapCoordinateSystem(rotationMatrix,
+                            SensorManager.AXIS_X,
+                            SensorManager.AXIS_Y,
+                            remappedRotationMatrix);
+                    SensorManager.getOrientation(remappedRotationMatrix, rotationData.orientations);
 
-                    TextView x_axis = (TextView)findViewById(R.id.x_axis_text),
-                            y_axis = (TextView)findViewById(R.id.y_axis_text),
-                            z_axis = (TextView)findViewById(R.id.z_axis_text);
+                    TextView x_axis = (TextView) findViewById(R.id.x_axis_text),
+                            y_axis = (TextView) findViewById(R.id.y_axis_text),
+                            z_axis = (TextView) findViewById(R.id.z_axis_text);
 //                            scalarRotation = (TextView)findViewById(R.id.scalar_rotation_text);
-                    x_axis.setText(String.valueOf(Math.toDegrees(attitude[1])));
-                    y_axis.setText(String.valueOf(Math.toDegrees(attitude[2])));
-                    z_axis.setText(String.valueOf(Math.toDegrees(attitude[0])));
+                    z_axis.setText(String.valueOf(Math.toDegrees(rotationData.orientations[0])));
+                    x_axis.setText(String.valueOf(Math.toDegrees(rotationData.orientations[1])));
+                    y_axis.setText(String.valueOf(Math.toDegrees(rotationData.orientations[2])));
 //                    scalarRotation.setText(String.valueOf(event.values[3]));
                 }
             }
 
             @Override
             public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
             }
         };
 
@@ -91,18 +95,18 @@ public class MainActivity extends Activity {
     private final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            if(ACTION_USB_PERMISSION.equals(intent.getAction())){
-                synchronized (this){
-                    usbDevice = (UsbDevice)intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
-                    if(intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)){
-                        if(usbDevice != null){
+            if (ACTION_USB_PERMISSION.equals(intent.getAction())) {
+                synchronized (this) {
+                    usbDevice = (UsbDevice) intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+                    if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+                        if (usbDevice != null) {
                             //TODO start communication
                         }
                     }
                 }
             }
-            if(UsbManager.ACTION_USB_DEVICE_DETACHED.equals(intent.getAction())){
-                if(usbDevice != null){
+            if (UsbManager.ACTION_USB_DEVICE_DETACHED.equals(intent.getAction())) {
+                if (usbDevice != null) {
                     //TODO close communication
                 }
             }
